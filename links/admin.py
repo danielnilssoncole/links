b7208c0b1285709fb7ad22e58294fe7b372d2855
daniel <dnilssoncole@gmail.com>
2017-08-07 07:08:52 -0400

    slug

--- links/admin.py
+++ links/admin.py
@@ -8,5 +8,12 @@ class PageInline(admin.TabularInline):
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [PageInline]
     list_display = ('name',)
+    search_fields = ['name']
+    prepopulated_fields = {'slug':('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    search_fields = ['title']
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- links/views.py
+++ links/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from links.models import Category, Page
 
 def index(request):
-    context = {'message': 'this is the message from the index view.'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context = {'categories': category_list}
     return render(request, 'links/index.html', context)
 
 def about(request):
