8f15128b9a4eefcc17715ddb4a0bbb95fda4b7c5
daniel <dnilssoncole@gmail.com>
2017-08-16 07:09:13 -0400

    add_category

--- links/views.py
+++ links/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from links.models import Category, Page
+from links.forms import CategoryForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -19,3 +20,17 @@ def show_category(request, category_name_slug):
     context = {'category': category,
                'pages': pages}
     return render(request, 'links/category.html', context)
+
+def add_catgegory(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    context = {'form': form}
+    return render(request, 'links/add_catgegory.html', context)
