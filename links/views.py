8c1abc5dab37ab6b7842f7da3f0e9ce3fbc3dbcb
daniel <dnilssoncole@gmail.com>
2017-10-14 07:12:43 -0400

    server side cookies

--- links/views.py
+++ links/views.py
@@ -9,17 +9,16 @@ from links.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from datetime import datetime
 
 def index(request):
-    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context = {'categories': category_list,
                'pages': page_list}
-    return render(request, 'links/index.html', context)
+    visitor_cookie_handler(request)
+    context['visits'] = request.session['visits']
+    response = render(request, 'links/index.html', context)
+    return response
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print('***TEST COOKIE WORKED***')
-        request.session.delete_test_cookie()
     context = {'name': 'daniel'}
     return render(request, 'links/about.html', context)
 
@@ -121,15 +120,21 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).days > 0:
         visits += 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
         visits = 1
-        response.set_cookie('last_visit', last_visit_cookie)
-    response.set_cookie('visits', visits)
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
